b57ef0aabbdff90d00dee40131135cf357112509
4rterius <4rterius@gmail.com>
2021-02-15 18:52:01 +0200

    Clear up the StructBinToObjHeaderText, even though it will be gone soon

--- UI/Converters/StructBinToObjHeaderText.cs
+++ UI/Converters/StructBinToObjHeaderText.cs
@@ -14,61 +14,25 @@ namespace NotInfiltrator.UI.Converters
 {
     public class StructBinToObjHeaderText : IValueConverter
     {
-        public class StructBinTempObjectData
+        public class ObjectData : Data
         {
-            public int Id { get; set; } = 0;
-            public byte[] OrigOffset { get; set; }
-            public uint Offset { get; set; } = 0;
+            public byte[] EncodedOffset { get; private set; }
             public uint Length { get; set; } = 0;
 
-            public StructBinTempObjectData(int id, uint offset, byte[] origOffset)
+            public uint Offset { get; private set; } = 0;
+
+            public ObjectData(int id, StructBin sbin, uint offset, byte[] encodedOffset)
+                : base(id, sbin)
             {
-                Id = id;
                 Offset = offset;
-                OrigOffset = origOffset;
+                EncodedOffset = encodedOffset;
             }
         }
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var sbin = value as Serialization.StructBin;
-            if (sbin is null)
-            {
-                return string.Empty;
-            }
-
-            var stream = sbin.Sections["OHDR"].NewMemoryStream();
-            var data = sbin.Sections["DATA"].Data;
-
-            var objs = new List<StructBinTempObjectData>();
-
-            var counter = 0;
-            while (stream.Position < stream.Length)
-            {
-                var origOffset = stream.ReadBytes(4);
-                var offset = DecodeOffset(origOffset);
-                if (counter > 0)
-                {
-                    objs[counter - 1].Length = offset - objs[counter - 1].Offset;
-                }
-                objs.Add(new StructBinTempObjectData(counter++, offset, origOffset));
-            }
-            objs[counter - 1].Length = (uint)data.Length - objs[counter - 1].Offset;
-
-            var stringBuilder = new StringBuilder();
-            stringBuilder.AppendLine($"Read {counter} indices...");
-            stringBuilder.AppendLine("\n");
-
-            foreach (var obj in objs)
-            {
-                stringBuilder.AppendLine($"Object 0x{obj.Id:X2}  @  0x{obj.Offset:X2}  (len = 0x{obj.Length:X2})  // orig. def. = {BitConverter.ToString(obj.OrigOffset)}");
-                stringBuilder.AppendLine();
-
-                stringBuilder.AppendLine(BitConverter.ToString(data.Skip((int)obj.Offset).Take((int)obj.Length).ToArray()).Replace("-", " "));
-                stringBuilder.AppendLine("\n");
-            }
-
-            return stringBuilder.ToString();
+            return sbin is null ? String.Empty : GetObjectText(sbin);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
@@ -88,6 +52,45 @@ namespace NotInfiltrator.UI.Converters
             res |= (UInt32)((UInt32)b[3] << (Int32)0x15);
 
             return res;
+        }
+
+        private static string GetObjectText(StructBin sbin)
+        {
+            // Build a list of temp. object datas
+
+            var objectDatas = new List<ObjectData>();
+
+            var headerStream = sbin.Sections["OHDR"].NewMemoryStream();
+            var objSectionData = sbin.Sections["DATA"].Data;
+
+            while (headerStream.Position < headerStream.Length)
+            {
+                var encodedOffset = headerStream.ReadBytes(4);
+                var offset = DecodeOffset(encodedOffset);
+                if (objectDatas.Count > 0)
+                {
+                    objectDatas[^1].Length = offset - objectDatas[^1].Offset;
+                }
+                objectDatas.Add(new(objectDatas.Count, sbin, offset, encodedOffset));
+            }
+            objectDatas[^1].Length = (uint)objSectionData.Length - objectDatas[^1].Offset;
+
+
+            // Build a string presentation
+
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"Read {objectDatas.Count} indices...\n");
+
+            foreach (var obj in objectDatas)
+            {
+                var range = (int)obj.Offset..(int)(obj.Offset + obj.Length);
+                var bytes = objSectionData[range];
+
+                stringBuilder.AppendLine($"Object 0x{obj.Id:X}  @  0x{obj.Offset:X}  (len = 0x{obj.Length:X})   // original offset = {BitConverter.ToString(obj.EncodedOffset)}\n");
+                stringBuilder.AppendLine(BitConverter.ToString(bytes).Replace('-', ' ') + "\n\n");
+            }
+
+            return stringBuilder.ToString();
         }
     }
 }
