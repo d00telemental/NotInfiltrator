84be077a0dab97512564494c87fbff1fc924117d
4rterius <4rterius@gmail.com>
2021-02-16 02:37:08 +0200

    Implement a basis for per-struct field listing

---
+++ UI/Converters/FieldChildKindTextConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+using NotInfiltrator.Serialization.Data;
+
+namespace NotInfiltrator.UI.Converters
+{
+    class FieldChildKindTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var field = value as FieldData ?? throw new NullReferenceException();
+            return field.Type switch
+            {
+                0x12 => field.StructBin.EnumDatas[field.ChildKind].Name,  // Enum
+                _ => field.ChildKind.ToString()
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
---
+++ UI/Converters/FieldChildKindVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+using NotInfiltrator.Serialization.Data;
+
+namespace NotInfiltrator.UI.Converters
+{
+    public class FieldChildKindVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var field = value as FieldData ?? throw new NullReferenceException();
+            return field.Type switch
+            {
+                0x10 => Visibility.Visible,  // InlineStruct
+                0x11 => Visibility.Visible,  // Array
+                0x12 => Visibility.Visible,  // Enum
+                _ => Visibility.Collapsed
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
